cb94ade1231de0c545d5805be7f10bfa6d7db186
Björn Petersen <git@bjoernpetersen.net>
2021-11-11 13:22:33 +0100

    Send reply silently

--- vredditbot.py
+++ vredditbot.py
@@ -74,6 +74,7 @@ def _send_drug_package(
         json={
             "chat_id": chat_id,
             "reply_to_message_id": reply_to_message_id,
+            "disable_notification": True,
             "media": [_build_input_media_video(drug) for drug in drugs]
         },
         timeout=10,
