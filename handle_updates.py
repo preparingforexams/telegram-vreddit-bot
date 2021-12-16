6f4ecedbc09bfe4852739b4f056e4caa4d3848db
Björn Petersen <git@bjoernpetersen.net>
2021-12-16 17:16:01 +0100

    Stringify payload

--- handle_updates.py
+++ handle_updates.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 import subprocess
@@ -232,7 +233,7 @@ def _publish_diagnosis(treatment: Treatment, diagnoses: List[Diagnosis]):
     topic = _get_topic(treatment)
     multiple(
         msgs=[
-            dict(topic=topic, payload=dict(debug=True, diagnosis=diagnosis.case))
+            dict(topic=topic, payload=json.dumps(dict(debug=True, diagnosis=diagnosis.case)))
             for diagnosis in diagnoses
         ],
         hostname=os.getenv("MQTT_HOST"),
