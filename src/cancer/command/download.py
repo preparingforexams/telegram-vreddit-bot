a12749897212678116ab76cfba8cc24694d1ecca
Björn Petersen <git@bjoernpetersen.net>
2022-01-18 19:22:51 +0100

    Allow dimensions up to 320p

--- src/cancer/command/download.py
+++ src/cancer/command/download.py
@@ -142,7 +142,7 @@ def _download_thumb(cure_dir: str, urls: List[str]) -> Optional[str]:
                 f.write(response.content)
 
             dimensions = _get_dimensions(thumb_path)
-            if max(dimensions) >= 320:
+            if max(dimensions) > 320:
                 _LOG.info(
                     "Skipping thumbnail %s because its dimensions (%d x %d) are too large",
                     url,
