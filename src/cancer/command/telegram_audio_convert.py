606eb2bcb38ac9daae07add64f3ce5efaedd39c2
Björn Petersen <git@bjoernpetersen.net>
2022-12-31 16:00:35 +0100

    Enable direct forward again

--- src/cancer/command/telegram_audio_convert.py
+++ src/cancer/command/telegram_audio_convert.py
@@ -1,7 +1,6 @@
 import logging
 import signal
 import sys
-import tempfile
 
 from cancer import telegram
 from cancer.adapter.pubsub import PubSubSubscriber, PubSubConfig
@@ -14,22 +13,11 @@ _LOG = logging.getLogger(__name__)
 def _handle_payload(payload: VoiceMessage) -> Subscriber.Result:
     _LOG.info("Received payload: %s", payload)
 
-    if payload.file_size > 20_000_000:
-        _LOG.info("Skipping because file is too large")
-        return Subscriber.Result.Ack
-
-    with tempfile.TemporaryFile() as file:
-        telegram.download_file(payload.file_id, file)
-
-        _LOG.info("Downloaded file of size %d", file.tell())
-
-        file.seek(0)
-
-        telegram.send_audio_message(
-            chat_id=payload.chat_id,
-            reply_to_message_id=payload.message_id,
-            audio=file,
-        )
+    telegram.send_audio_message(
+        chat_id=payload.chat_id,
+        reply_to_message_id=payload.message_id,
+        audio=payload.file_id,
+    )
 
     return Subscriber.Result.Ack
 
