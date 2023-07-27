e25f07e6def2210e33d5f2dfad7e335bd6a41694
Björn Petersen <git@bjoernpetersen.net>
2023-07-27 15:33:35 +0200

    Fix test

--- src/tests/command/test_download.py
+++ src/tests/command/test_download.py
@@ -3,5 +3,5 @@ from cancer.command import download
 
 def test_check_size():
     url = "https://www.youtube.com/watch?v=eomhW5MLGWA"
-    size = download._check_size(url)
-    assert 4_000_000 < size < 6_000_000
+    info = download._get_info(url)
+    assert 4_000_000 < info.size < 6_000_000
